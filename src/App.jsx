1022df58e359a5efd2499f1c9c300cbdd002b591
senakaratss <karatasssena@gmail.com>
2025-08-11 16:39:50 +0300

    products and users page completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import OverviewPage from "./pages/OverviewPage";
 import ProductsPage from "./pages/ProductsPage";
+import UsersPage from "./pages/UsersPage";
 import Sidebar from "./components/Sidebar";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route path="/" element={<OverviewPage />} />
         <Route path="/products" element={<ProductsPage />} />
+        <Route path="/users" element={<UsersPage />} />
       </Routes>
     </div>
   );
